a492496156b6d44c4042a92e20dd23a84ceac128
sudarshan233 <nsridharsudarshan@gmail.com>
2026-02-07 12:08:15 +0530

    Changed Initial Configs and Installed Air

--- routes/router.go
+++ routes/router.go
@@ -1,17 +1,17 @@
 package routes
 
 import (
-    "fs-backend/http/controllers"
-    "fs-backend/services"
-    "github.com/gin-gonic/gin"
+	"fs-backend/http/controllers"
+	"fs-backend/services"
+	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine, postService services.PostService) {
-    postController := controllers.NewPostController(postService)
+	postController := controllers.NewPostController(postService)
 
-    api := router.Group("/api")
-    {
-        api.POST("/posts", postController.Create)
-        api.GET("/posts", postController.GetAll)
-    }
+	api := router.Group("/api/v1")
+	{
+		api.POST("/posts", postController.Create)
+		api.GET("/posts", postController.GetAll)
+	}
 }
